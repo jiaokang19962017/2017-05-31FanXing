f76407e3b7467f86deb59d7f76b6a4ad316d045a
jiaokang <Administrator@HP201-1>
2017-05-31 09:55:20 +0800

    泛型

--- ConsoleApplication1/Class1.cs
+++ ConsoleApplication1/Class1.cs
@@ -6,23 +6,32 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Class1
+    /// <summary>
+    /// 数组类
+    /// 泛型类,T可以是任何类型
+    /// 使用时需要为T指定数据类型
+    /// 泛型在编译时不指定数据类型,调用时才指定数据类型
+    /// 使用泛型可以实现任何类型的数组
+    /// 泛型的优点:1.避免了装箱和拆箱的问题 2.保护了类型安全 
+    /// 3.提高了程序性能 4.提高代码的复用性
+    /// </summary>
+    class Class1<T>
     {
-        private int[] array;//定义一个数组
+        private T[] array;//定义一个数组
         private int count = 0;//数组下标
-        public void Add(int value)//数组添加元素的方法
+        public void Add(T value)//数组添加元素的方法
         {
             array[count] = value;
             count++;
         }
-        public int this[int index]//索引器
+        public T this[int index]//索引器
         {
             get { return array[index]; }
             set { array[index] = value; }
         }
         public Class1()//构造函数
         {
-            array = new int[50];
+            array = new T[50];
         }
        
         public void PrintLog()
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,11 +10,17 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Class1 c = new Class1();
-            c.Add(8);
-            c.Add(9);
-            c.Add(10);
-            c.PrintLog();
+            /* Class1 c = new Class1();
+             c.Add(8);
+             c.Add(9);
+             c.Add(10);
+             c.PrintLog();*/
+
+            Class1<int> array = new Class1<int>();
+            array.Add(12);
+            Class1<string> array1 = new Class1<string>();
+            array1.Add("字符串");
+            array1.PrintLog();
         }
     }
 }
